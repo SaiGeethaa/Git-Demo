09d1bb5b52716e75fb51794a423c240d7b767d8b
Home <Home@192.168.62.1>
2018-09-23 09:06:45 +0530

    commit1

---
+++ HandlingGit/src/TestGit.java
@@ -0,0 +1,18 @@
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Helloo world For GIT  !!!!");
+		
+		System.out.println("Checking for Update  1......");
+
+		System.out.println("Checking for Update  2...... ");
+		
+		System.out.println("Checking for Update  3...... Auto Build");
+
+
+	}
+
+}

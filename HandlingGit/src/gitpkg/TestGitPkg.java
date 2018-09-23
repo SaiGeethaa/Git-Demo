8f216a7a153f305d239974c160d59ae6a9b96f7c
SaiGeethaa <geet1685@gmail.com>
2018-09-23 10:53:51 +0530

    commit pkg

--- HandlingGit/src/gitpkg/TestGitPkg.java
+++ HandlingGit/src/gitpkg/TestGitPkg.java
@@ -1,13 +1,17 @@
+package gitpkg;
 
 
-public class TestGit {
+public class TestGitPkg {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Helloo world For GIT inside PKG !!!!");
+		System.out.println("Helloo world For GIT  !!!!");
 
 		System.out.println("Checking for Update  1......");
+
+		System.out.println("Checking for Update  2......");
+
 
 		}
 
